034d8908d3470fbcfe8a5812506df78b6b9d05fb
okuharaRR <okuhara.sotsuken@gmail.com>
2020-03-12 20:14:28 +0000

    Real Time sensor was implemented in .h and .cpp files

--- test/RealTime_Switch2/RealTime_Switch/RealTime_Switch.h
+++ test/RealTime_Switch2/RealTime_Switch/RealTime_Switch.h
@@ -3,6 +3,33 @@
 #ifndef SWITCH_H
 #define SWITCH_H
 
+#include "CppTimer.h"
 
+class Relay : public CppTimer {
+
+public:
+
+	void on();
+	
+	void timerEvent();
+};
+	
+class Valve1 : public CppTimer {
+	
+ public:
+ 
+	void on();
+	
+	void timerEvent();
+};
+	
+class Valve2 : public CppTimer {
+
+ public:
+ 
+	void on();
+	
+	void timerEvent();
+};
 
 #endif 
